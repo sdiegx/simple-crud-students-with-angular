69255cae50b9f8d5bc6a799cfb4ec5dce7242bac
sdiegx <zebastian7616@gmail.com>
2025-01-08 00:18:32 -0500

    Agregando componente formulario de estudiantes y agregando las rutas a los archivos de configuracion. Llamando las funciones desde app.component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { StudentListComponent } from './components/student-list/student-list.component';
+import { StudentFormComponent } from './components/student-form/student-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: StudentListComponent },
+  { path: 'add', component: StudentFormComponent },
+];
--- src/app/components/student-form/student-form.component.ts
+++ src/app/components/student-form/student-form.component.ts
@@ -1,12 +1,44 @@
 import { Component } from '@angular/core';
+import { StudentService } from '../../services/student.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-student-form',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './student-form.component.html',
-  styleUrl: './student-form.component.css'
+  styleUrl: './student-form.component.css',
 })
 export class StudentFormComponent {
+  student: any = {
+    student_id: '',
+    name: '',
+    age: 0,
+    course: 0,
+    group: 0,
+    subject: '',
+    email: '',
+    phone: '',
+    adress: '',
+    placeWork: '',
+  };
+  constructor(private studentService: StudentService) {}
 
+  addStudent() {
+    this.studentService.addStudent(this.student).subscribe(() => {
+      this.student = {
+        student_id: '',
+        name: '',
+        age: 0,
+        course: 0,
+        group: 0,
+        subject: '',
+        email: '',
+        phone: '',
+        adress: '',
+        placeWork: '',
+      };
+    });
+  }
 }
